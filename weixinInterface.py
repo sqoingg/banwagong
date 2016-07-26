e6a254d9c39ee2d318298a61021de66e7b113845
pangzhi <704526840@qq.com>
2016-07-26 16:29:07 +0800

    make it better

--- weixinInterface.py
+++ weixinInterface.py
@@ -10,7 +10,7 @@ class WeixinInterface:
 
     def __init__(self):
         self.app_root = os.path.dirname(__file__)
-        self.templates_root = os.path.join(self.app_root, 'templates')
+        self.templates_root = os.path.join(self.app_root, "templates")
         self.render = web.template.render(self.templates_root)
 
     def GET(self):
