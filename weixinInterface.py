b16702d434be7af7d884d641e639bfd609ab9b5b
pangzhi <704526840@qq.com>
2016-07-28 20:58:45 +0800

    make it better

--- weixinInterface.py
+++ weixinInterface.py
@@ -6,6 +6,7 @@ import time
 import os
 import urllib2,json
 from lxml import etree 
+import json
 
 class WeixinInterface:
 
@@ -41,4 +42,18 @@ class WeixinInterface:
         msgType=xml.find("MsgType").text
         fromUser=xml.find("FromUserName").text
         toUser=xml.find("ToUserName").text
-        return self.render.reply_text(fromUser,toUser,int(time.time()),u"我现在还在开发中，还没有什么功能，您刚才说的是："+content)
+        def youdao(word):
+            qword = urllib2.quote(word)
+            baseurl = r'http://fanyi.youdao.com/openapi.do?keyfrom=fanyii&key=1281815706&type=data&doctype=json&version=1.1&q='
+            url = baseurl+qword
+            resp = urllib2.urlopen(url)
+            fanyi = json.loads(resp.read())
+            ##根据json是否返回一个叫“basic”的key来判断是否翻译成功
+            if 'basic' in fanyi.keys():
+                ##下面是你自已来组织格式
+                trans = u'%s:\n%s\n%s\n网络释义：\n%s'%(fanyi['query'],''.join(fanyi['translation']),''.join(fanyi['basic']['explains']),''.join(fanyi['web'][0]['value']))
+                return trans
+            else:
+                return u'对不起，您输入的单词%s无法翻译，请检查拼写'% word
+        Nword = youdao(content)        
+        return self.render.reply_text(fromUser,toUser,int(time.time()),Nword)
